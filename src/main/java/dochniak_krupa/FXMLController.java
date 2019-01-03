a3a379ebd835593ba69501ef2501393fdc1f5b9e
Jakub Dochniak <jakub.dochniak@protonmail.com>
2019-01-03 15:06:49 +0100

    Create JavaFX project from Maven Archatype

---
+++ src/main/java/dochniak_krupa/FXMLController.java
@@ -0,0 +1,17 @@
+package dochniak_krupa;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class FXMLController {
+    
+    @FXML
+    private Label label;
+    
+    @FXML
+    private void handleButtonAction(ActionEvent event) {
+        System.out.println("You clicked me!");
+        label.setText("Hello World!");
+    }
+}
